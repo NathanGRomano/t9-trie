b778b0c65b488d9b569dc1337aa1b4c81d8dfda9
Nathan <nathan.g.romano@gmail.com>
2013-08-27 20:12:35 +0000

    forgot to add done()

--- test/node.js
+++ test/node.js
@@ -6,7 +6,7 @@ describe('given a node', function () {
 
 	describe('when adding a child given a key', function () {
 
-		it('it should be stored by the key', function () {
+		it('it should be stored by the key', function (done) {
 
 			root.add( new Node('2', 'Two'), function (err, node) {
 				assert.equal(err, null);
@@ -20,7 +20,7 @@ describe('given a node', function () {
 
 	describe('when asked if we have children given a key', function () {
 
-		it('should return a list given the key', function () {
+		it('should return a list given the key', function (done) {
 
 			root.get( '2', function (err, node) {
 
