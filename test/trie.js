76e75bbd03104f9f39aa656df4f7372f6d0678cd
Nathan <nathan.g.romano@gmail.com>
2013-08-27 22:08:45 +0000

    added a test for finding a three character word

--- test/trie.js
+++ test/trie.js
@@ -54,5 +54,20 @@ describe('given a trie', function () {
 
 	});
 
+	describe('when asked to find a three character word', function () {
+
+		it('then it should be returned', function (done) {
+
+			trie.find('sit', function (err, node) {
+				assert.equal(err, null);
+				assert.equal(node instanceof Node, true);
+				assert.equal(node.toString(), 'sit');
+				done();
+			});
+
+		});
+
+	});
+
 });
 
