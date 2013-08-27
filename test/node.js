40793e825d9082e0c9c79b1ee6866b3004ef396e
Nathan <nathan.g.romano@gmail.com>
2013-08-27 21:46:58 +0000

    modified test criteria

--- test/node.js
+++ test/node.js
@@ -1,6 +1,6 @@
 var assert = require('assert')
   , Node = require('../lib/node')
-  , root = new Node('1', 'One');
+  , root = new Node('', '');
 
 describe('given a node', function () {
 
@@ -8,7 +8,7 @@ describe('given a node', function () {
 
 		it('it should be stored by the key', function (done) {
 
-			root.add( new Node('2', 'Two'), function (err, node) {
+			root.add( new Node('T', 'T'), function (err, node) {
 				assert.equal(err, null);
 				assert.equal(node instanceof Node, true);
 				done();
@@ -22,14 +22,11 @@ describe('given a node', function () {
 
 		it('should return a list given the key', function (done) {
 
-			root.get( '2', function (err, node) {
-
+			root.get('T', function (err, node) {
 				assert.equal(err, null);
-				assert.equal(node instanceof Array, true);
-				assert.equal(node[0] instanceof Node, true);
-				assert.equal(node[0].key, '2');
+				assert.equal(node instanceof Node, true);
+				assert.equal(node.key, 'T');
 				done();
-
 			});
 
 		});
