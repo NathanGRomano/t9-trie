27ccbbaa3ff5209b0660cc280c4affcf18ebd48d
Nathan <nathan.g.romano@gmail.com>
2013-08-27 21:49:22 +0000

    modified the test for adding a value and added a test for adding a three character string and modified the assertions for the tests

--- test/trie.js
+++ test/trie.js
@@ -1,34 +1,52 @@
 var assert = require('assert')
   , Trie = require('../lib/trie')
 	, Node = require('../lib/node')
-  , trie = new Trie()
 
 describe('given a trie', function () {
+	
+	var trie = new Trie();
 
-	describe('when adding a node', function () {
+	describe('when adding a single character', function () {
 
-		it('then should be added', function (done) {
+		it('then it should be added', function (done) {
 			
-			trie.add(new Node('some value'), function (err, node) {
+			trie.add('s', function (err, node) {
 
 				assert.equal(err, null);
 				assert.equal(node instanceof Node, true);
-				assert.equal(node.value === 'some value', true);
+				assert.equal(node.toString(), 's');
 				done();
 
 			});
 		});
 
 	});
-	
-	describe('when asked to find a node', function () {
 
-		it('then should be returned', function () {
+	describe('when adding a three character word', function () {
+		
+		it('then it should be added', function (done) {
 
-			trie.find('some key', function (err, node) {
+			trie.add('sit', function (err, node) {
+
 				assert.equal(err, null);
 				assert.equal(node instanceof Node, true);
-				assert.equal(node.value === 'some value', true);
+				assert.equal(node.toString(), 'sit');
+				done();
+
+			});
+
+		});
+
+	});
+	
+	describe('when asked to find the single character', function () {
+
+		it('then it should be returned', function () {
+
+			trie.find('s', function (err, node) {
+				assert.equal(err, null);
+				assert.equal(node instanceof Node, true);
+				assert.equal(node.toString(), 's');
 				done();
 			});
 
